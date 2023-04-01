1af0b67a8d94d85d6a9e0b9621ded6121629d794
saviour89 <benjamincgabriel@gmail.com>
2023-03-31 22:07:50 -0400

    admin

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 from sqlalchemy.sql import func, text
+from sqlalchemy import Delete, Update, Insert
 
 from pypika import (
     AliasedQuery,
     Case,
     ClickHouseQuery,
     EmptyCriterion,
-    Field as F,
+    Field,
     Index,
     MSSQLQuery,
     MySQLQuery,
@@ -126,10 +127,169 @@ def rooms():
 
 @app.route("/admin", methods=["GET", "POST"])
 def admin():
-    if request.method == "POST":
-        
-        return render_template("admin.html")
     return render_template("admin.html")
+
+
+@app.route("/deletehotelchain", methods=["GET", "POST"])
+def deletehotelchain():
+    if request.method == "POST":
+        r, has, hotel, owns, hotelchain = Table('room'), Table('has'), Table('hotel'), Table('owns'), Table('hotelchain')
+        hotelchaindelete = request.form.get('hotelchaindelete')
+
+        if hotelchaindelete!="none":
+            query = Query.from_(hotelchain).delete().where(hotelchain.chainname==hotelchaindelete)
+            print(query)
+            db.session.execute(text(str(query)))
+            db.session.commit()
+
+        hotelchains = db.session.execute(text('select * from hotelchain')).mappings().all()
+        return render_template("deletehotelchain.html", hotelchains=hotelchains, numhotelchains=len(hotelchains))
+    
+    hotelchains = db.session.execute(text('select * from hotelchain')).mappings().all()
+    return render_template("deletehotelchain.html", hotelchains=hotelchains, numhotelchains=len(hotelchains))
+
+
+@app.route("/deletecustomer", methods=["GET", "POST"])
+def deletecustomer():
+    if request.method == "POST":
+        customer = Table('customer')
+        customerdelete = request.form.get('customerdelete')
+
+        if customerdelete!="none":
+            query = Query.from_(customer).delete().where(customer.customername==customerdelete)
+            print(query)
+            db.session.execute(text(str(query)))
+            db.session.commit()
+
+        customers = db.session.execute(text('select * from customer')).mappings().all()
+        return render_template("deletecustomer.html", customers=customers, numcustomers=len(customers))
+    
+    customers = db.session.execute(text('select * from customer')).mappings().all()
+    return render_template("deletecustomer.html", customers=customers, numcustomers=len(customers))
+
+
+@app.route("/deleteemployee", methods=["GET", "POST"])
+def deleteemployee():
+    if request.method == "POST":
+        employee = Table('employee')
+        employeedelete = request.form.get('employeedelete')
+
+        if employeedelete!="none":
+            query = Query.from_(employee).delete().where(employee.employeename==employeedelete)
+            print(query)
+            db.session.execute(text(str(query)))
+            db.session.commit()
+
+        employees = db.session.execute(text('select * from employee')).mappings().all()
+        return render_template("deleteemployee.html", employees=employees, numemployees=len(employees))
+    
+    employees = db.session.execute(text('select * from employee')).mappings().all()
+    return render_template("deleteemployee.html", employees=employees, numemployees=len(employees))
+
+
+@app.route("/deletehotel", methods=["GET", "POST"])
+def deletehotel():
+    if request.method == "POST":
+        hotel = Table('hotel')
+        hoteldelete = request.form.get('hoteldelete')
+
+        if hoteldelete!="none":
+            query = Query.from_(hotel).delete().where(hotel.hotelname==hoteldelete)
+            print(query)
+            db.session.execute(text(str(query)))
+            db.session.commit()
+
+        hotels = db.session.execute(text('select * from hotel')).mappings().all()
+        return render_template("deletehotel.html", hotels=hotels, numhotels=len(hotels))
+    
+    hotels = db.session.execute(text('select * from hotel')).mappings().all()
+    return render_template("deletehotel.html", hotels=hotels, numhotels=len(hotels))
+
+
+@app.route("/deleteroom", methods=["GET", "POST"])
+def deleteroom():
+    if request.method == "POST":
+        room = Table('room')
+        roomdelete = request.form.get('roomdelete')
+
+        if roomdelete!="none":
+            query = Query.from_(room).delete().where(room.roomid==roomdelete)
+            print(query)
+            db.session.execute(text(str(query)))
+            db.session.commit()
+
+        rooms = db.session.execute(text('select * from room')).mappings().all()
+        return render_template("deleteroom.html", rooms=rooms, numrooms=len(rooms))
+    
+    rooms = db.session.execute(text('select * from room')).mappings().all()
+    return render_template("deleteroom.html", rooms=rooms, numrooms=len(rooms))
+
+
+@app.route("/addcustomer", methods=["GET", "POST"])
+def addcustomer():
+    if request.method == "POST":
+        customer = Table('customer')
+        
+        query = Query.into(customer).insert(request.form.get('ssn'), request.form.get('name'), request.form.get('address'), request.form.get('date'))
+        print(query)
+        db.session.execute(text(str(query)))
+        db.session.commit()
+
+        return render_template("addcustomer.html")
+    
+    return render_template("addcustomer.html")
+
+
+@app.route("/addemployee", methods=["GET", "POST"])
+def addemployee():
+    if request.method == "POST":
+        employee = Table('employee')
+        
+        query = Query.into(employee).insert(request.form.get('ssn'), request.form.get('name'), request.form.get('address'), request.form.get('role'))
+        print(query)
+        db.session.execute(text(str(query)))
+        db.session.commit()
+
+        return render_template("addemployee.html")
+    
+    return render_template("addemployee.html")
+
+
+@app.route("/addhotel", methods=["GET", "POST"])
+def addhotel():
+    if request.method == "POST":
+        hotel = Table('hotel')
+        
+        query = Query.into(hotel).insert(request.form.get('id'), request.form.get('name'), request.form.get('phonenumber'), request.form.get('contactemail'), int(request.form.get('rating')), request.form.get('address'))
+        print(query)
+        db.session.execute(text(str(query)))
+        db.session.commit()
+
+        return render_template("addhotel.html")
+    
+    return render_template("addhotel.html")
+
+
+@app.route("/addroom", methods=["GET", "POST"])
+def addroom():
+    if request.method == "POST":
+        room = Table('room')
+
+        tv = request.form.get('tv')=="on"
+        fridge = request.form.get('fridge')=="on"
+        freezer = request.form.get('freezer')=="on"
+        ac = request.form.get('ac')=="on"
+        ext = request.form.get('ext')=="on"
+        dmg = request.form.get('dmg')=="on"
+        
+        query = Query.into(room).insert(int(request.form.get('id')), ext, int(request.form.get('roomnumber')), int(request.form.get('price')), int(request.form.get('capacity')), ac, tv, fridge, freezer, dmg, str(request.form.get('view')))
+        print(query)
+        db.session.execute(text(str(query)))
+        db.session.commit()
+
+        return render_template("addroom.html")
+    
+    return render_template("addroom.html")
 
 
 if __name__ == "__main__":
